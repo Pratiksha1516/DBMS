8ed57d26a0f2bf4e3da0edb3731428b75b5a3790
Pratiksha1516 <165662273+Pratiksha1516@users.noreply.github.com>
2025-03-12 00:00:09 +0530

    Add files via upload
    this is something interesting

---
+++ dbms4.sql
@@ -0,0 +1,10 @@
+create table stud(
+    stud_name varchar(20),
+    stud_contact int
+);
+
+INSERT INTO stud VALUES  ('rita', 524681354);
+INSERT INTO stud VALUES   ('ram', 5468762164);
+
+
+SELECT stud_name FROM stud WHERE stud_name LIKE '___a%';
