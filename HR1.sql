54cfc4fc3e5d268ba90cab5be3dbbba0f90e1916
Pratiksha1516 <165662273+Pratiksha1516@users.noreply.github.com>
2025-03-20 18:04:54 +0530

    Add files via upload

---
+++ HR1.sql
@@ -0,0 +1,21 @@
+create table setA(
+element int
+);
+
+create table setB(
+element int
+);
+
+insert into setA(element) values
+(1), (2), (3), (4), (5), (6);
+
+insert into setB(element) values
+(2), (3), (4), (5), (6), (7), (8);
+
+select count(element)
+from(
+	select element from setA
+	UNION
+	select element from setB
+) AS UnionResult;
+
---
+++ HR4.sql
@@ -0,0 +1,17 @@
+create table setA(
+element int
+);
+
+create table setB(
+element int
+);
+
+insert into setA(element) values
+(1), (2), (3), (4), (5), (6);
+
+insert into setB(element) values
+(2), (3), (4), (5), (6), (7), (8);
+
+SELECT COUNT(*)
+FROM setA
+cross join setB;
