2bb7d29cbf92f2a8af95a56c19d7f59ed732bdd3
Pratiksha Prakash Patil <165662273+Pratiksha1516@users.noreply.github.com>
2025-04-06 11:24:28 +0530

    Update dbms13.sql

--- dbms13.sql
+++ dbms13.sql
@@ -1,4 +1,3 @@
--- 1. Create Customer Table and Invoice Table
 CREATE TABLE Customer (
     id VARCHAR(5) PRIMARY KEY,
     fname VARCHAR(50),
@@ -13,28 +12,26 @@ CREATE TABLE Invoice (
     FOREIGN KEY (Cust_id) REFERENCES Customer(id)
 );
 
--- Insert values into Customer Table
 INSERT INTO Customer VALUES('A01', 'Allan', 'Border', 'SA', '723622');
 INSERT INTO Customer VALUES('A02', 'Tina', 'Shields', 'MO', '23784');
 INSERT INTO Customer VALUES('A03', 'Ravi', 'Kumar', 'BI', '545621');
 INSERT INTO Customer VALUES('A04', 'Sunita', 'Rai', 'CH', '983724');
 INSERT INTO Customer VALUES('A05', 'James', 'Smith', 'WA', '634672');
 
--- Insert values into Invoice Table
 INSERT INTO Invoice VALUES(101, 'A01');
 INSERT INTO Invoice VALUES(102, 'A02');
 INSERT INTO Invoice VALUES(103, 'A03');
 INSERT INTO Invoice VALUES(104, 'A04');
 INSERT INTO Invoice VALUES(105, 'A05');
 
--- 2. Print entire Customer table
 SELECT * FROM Customer;
 
--- 3. List of fname and area of all customers
 SELECT fname, area FROM Customer;
 
--- 4. Find customers having 'a' as the second letter in fname
 SELECT * FROM Customer WHERE fname LIKE '_a%';
 
--- 5. Find customers staying in area SA, BI, or CH
 SELECT * FROM Customer WHERE area IN ('SA', 'BI', 'CH');
+
+SELECT fname
+FROM Customer
+WHERE id = 'A03';
