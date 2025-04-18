953b7e35098f91a43158f334f9ea8ad17b5705e2
Pratiksha Prakash Patil <165662273+Pratiksha1516@users.noreply.github.com>
2025-04-18 09:01:22 +0530

    Add files via upload

---
+++ dbms27.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE college1;
+
+CREATE TABLE IF NOT EXISTS student2(
+rollno INT PRIMARY KEY,
+name VARCHAR(40),
+marks INT NOT NULL,
+grade VARCHAR(1),
+city VARCHAR(20)
+);
+
+INSERT INTO student2
+(rollno, name, marks, grade, city)
+VALUES
+(101, 'anil', 78, 'C', 'pune'),
+(102, 'bhumika', 93, 'A', 'Mumbai'),
+(103, 'chetan', 85, 'B', 'Delhi'),
+(104, 'dhruv', 96, 'A', 'Sangli');
+
+SELECT name, marks FROM student2;
+
+SELECT * FROM student2;
+
+SELECT DISTINCT city FROM student2;
+
+SELECT * 
+FROM student2 
+WHERE marks > 80;
+
+SELECT *
+FROM student2
+WHERE marks+10 > 100;
+
