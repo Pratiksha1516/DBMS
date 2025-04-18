ae142ef359cf7ce2b453ab700e464ca5c70bee43
Pratiksha Prakash Patil <165662273+Pratiksha1516@users.noreply.github.com>
2025-04-18 07:59:34 +0530

    Add files via upload

---
+++ dbms22.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE xyz_company;
+
+CREATE TABLE IF NOT EXISTS employee2(
+id INT PRIMARY KEY,
+name VARCHAR (50),
+salary INT
+);
+
+INSERT INTO employee2 (id, name, salary)
+VALUES
+(1, 'ADAM', 25000),
+(2, 'BOB', 30000),
+(3, 'CASEY', 40000);
+
+SELECT * FROM employee2;
