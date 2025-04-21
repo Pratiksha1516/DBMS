f480a19cfe9e36dd56336e50ec4c880f3ba8006c
Pratiksha Prakash Patil <165662273+Pratiksha1516@users.noreply.github.com>
2025-04-22 01:23:39 +0530

    Add files via upload

---
+++ dbms43.sql
@@ -0,0 +1,21 @@
+CREATE TABLE employee(
+id INT PRIMARY KEY,
+name VARCHAR(40),
+manager_id INT
+);
+
+INSERT INTO employee
+(id, name, manager_id)
+VALUES
+(101, 'adam', 103),
+(102, 'bob', 104),
+(103, 'casey', NULL),
+(104, 'donald', 103);
+
+SELECT * FROM employee;
+
+--self join(it joins data within one table )
+SELECT e.name as manager_name, f.name
+FROM employee as e
+JOIN employee as f   
+ON e.id = f.manager_id;
