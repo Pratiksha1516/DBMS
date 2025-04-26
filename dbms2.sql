7cbbf58f239fe36e28009e4ada119c98ba52cb82
Pratiksha Prakash Patil <165662273+Pratiksha1516@users.noreply.github.com>
2025-04-26 22:55:14 +0530

    Update dbms2.sql

--- dbms2.sql
+++ dbms2.sql
@@ -10,6 +10,11 @@ INSERT INTO emp VALUES  (1, 'prati', 20, 1000000, 'database');
 INSERT INTO emp VALUES   (2, 'ranveer', 20, 90000000, 'full stack');
 INSERT INTO emp VALUES   (3, 'shiri', 20, 90000000, 'designing');
 
+
+--Aggregate functions
+SELECT COUNT(emp_id)
+FROM emp;
+
 select * from emp where emp_dep = 'full stack';
 
 select * from emp where emp_salary > 100000;
