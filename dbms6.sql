279faa7587090132347e66c9eae25c52ad4b570c
Pratiksha Prakash Patil <165662273+Pratiksha1516@users.noreply.github.com>
2025-04-30 23:21:48 +0530

    Update dbms6.sql

--- dbms6.sql
+++ dbms6.sql
@@ -15,6 +15,10 @@ VALUES
 (3, 'ghi', 'ghi@gmail.com', '1122334455', 'Chicago', TO_DATE('2023-02-10', 'YYYY-MM-DD'));
 
 SELECT * FROM customers 
+
+--Aggregate functions
+
+    
 WHERE registration_date >= TO_DATE('2022-01-01', 'YYYY-MM-DD');
 
 SELECT * FROM  customers
