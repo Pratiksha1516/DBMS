8db358f62356473c8351f83f1b8d2a8bfc031880
Pratiksha Prakash Patil <165662273+Pratiksha1516@users.noreply.github.com>
2025-05-07 21:23:08 +0530

    Update HR3.sql

--- HR3.sql
+++ HR3.sql
@@ -26,6 +26,13 @@ FROM(
 	SELECT element FROM setB
 )AS combined;
 
+SELECT MAX(element)
+FROM(
+	SELECT element FROM setA
+	UNION ALL
+	SELECT element FROM setB
+)AS combined;
+
 drop table setA;
 
 drop table setB;
