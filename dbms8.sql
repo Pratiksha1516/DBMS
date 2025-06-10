5fa68191bb1df7e971e14a86e946bbe81606ef82
Pratiksha Prakash Patil <165662273+Pratiksha1516@users.noreply.github.com>
2025-06-10 23:14:56 +0530

    Update dbms8.sql

--- dbms8.sql
+++ dbms8.sql
@@ -53,6 +53,8 @@ SELECT Name, Department FROM Student WHERE Age > 20;
 
 SELECT StudentID, Grade FROM Enrollment;
 
+SELECT CourseID, Semester FROM Enrollment;
+
 select S.name
 from student s
 join Enrollment E on S.studentID = E.studentID
