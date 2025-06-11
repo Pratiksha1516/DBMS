ab05ec8b9e1a0e810184b5169f0acb26f36a6a4f
Pratiksha Prakash Patil <165662273+Pratiksha1516@users.noreply.github.com>
2025-06-11 13:56:50 +0530

    Update dbms9.sql

--- dbms9.sql
+++ dbms9.sql
@@ -10,14 +10,12 @@ CREATE TABLE Product (
     Price DECIMAL(10, 2)
 );
 
-INSERT INTO Customer (CustomerID, Name, Email) VALUES (1, 'Ranveer', 'ran@example.com');
-INSERT INTO Customer (CustomerID, Name, Email) VALUES (2, 'pratiksha', 'prati@example.com');
-INSERT INTO Customer (CustomerID, Name, Email) VALUES (3, 'shiri', 'shiri@example.com');
+INSERT INTO Customer (CustomerID, Name, Email)
+VALUES 
+(1, 'Ranveer', 'ran@example.com'),
+(2, 'pratiksha', 'prati@example.com'),
+(3, 'shiri', 'shiri@example.com');
 
-INSERT INTO Product (ProductID, ProductName, Price) VALUES (1, 'Laptop', 1500.00);
-INSERT INTO Product (ProductID, ProductName, Price) VALUES (2, 'Phone', 800.00);
-INSERT INTO Product (ProductID, ProductName, Price) VALUES (3, 'Tablet', 500.00);
-INSERT INTO Product (ProductID, ProductName, Price) VALUES (4, 'Headphones', 200.00);
 
 select * from customer
     
