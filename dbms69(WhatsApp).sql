f43b98229755f5f8460db4a50b925943a92d738b
Pratiksha Prakash Patil <165662273+Pratiksha1516@users.noreply.github.com>
2025-08-25 23:31:14 +0530

    Update dbms69(WhatsApp).sql

--- dbms69(WhatsApp).sql
+++ dbms69(WhatsApp).sql
@@ -84,6 +84,9 @@ INSERT INTO MessageStatus (MessageID, UserID, Status, SeenAt) VALUES
 (1005, 4, 'delivered', NULL),
 (1005, 5, 'seen', '2025-08-22 07:54:00');
 
+SELECT MessageID, ChatID, Content 
+FROM Messages;
+
 SELECT ChatID 
 FROM Chats 
 WHERE IsGroup = FALSE;
@@ -203,6 +206,7 @@ SELECT u.Name, COUNT(DISTINCT cm.ChatID) AS TotalChats
 FROM Users u
 JOIN ChatMembers cm ON u.UserID = cm.UserID
 GROUP BY u.Name;
+
 
 
 
