247e56a0818973750152b75f5f6648d019d8c32e
Pratiksha Prakash Patil <165662273+Pratiksha1516@users.noreply.github.com>
2025-10-02 13:34:49 +0530

    Update dbms70(e-commerce).sql

--- dbms70(e-commerce).sql
+++ dbms70(e-commerce).sql
@@ -5,3 +5,10 @@ CREATE TABLE Product (
     price DECIMAL(10,2),
     stock INT
 );
+
+CREATE TABLE Supplier (
+    supplier_id INT PRIMARY KEY,
+    supplier_name VARCHAR(50),
+    city VARCHAR(30),
+    contact VARCHAR(15)
+);
