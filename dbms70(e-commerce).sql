611e8a76104842c944882ce2e7ed83c8061ced63
Pratiksha Prakash Patil <165662273+Pratiksha1516@users.noreply.github.com>
2025-10-02 13:39:22 +0530

    Update dbms70(e-commerce).sql

--- dbms70(e-commerce).sql
+++ dbms70(e-commerce).sql
@@ -111,4 +111,11 @@ FROM OrderDetails od
 JOIN Product p ON od.product_id = p.product_id
 GROUP BY p.product_name;
 
+SELECT c.customer_name, SUM(o.total_amount) AS total_spent
+FROM Orders o
+JOIN Customer c ON o.customer_id = c.customer_id
+GROUP BY c.customer_name
+ORDER BY total_spent DESC
+LIMIT 3;
+
 
